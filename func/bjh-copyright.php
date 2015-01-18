77c1273312a8c10311efcfd0c23a5bc9a4c5cfb1
bjhee <junhe0526@hotmail.com>
2015-01-19 03:30:43 +0800

    Implement the basic function for copyright

--- func/bjh-copyright.php
+++ func/bjh-copyright.php
@@ -7,6 +7,9 @@ class BsaCopyright {
     const OPTION_COPYRIGHT = 'bsa_copyright';
     const OPTION_INFO = 'bsa_copyright_info';
     
+    // Reserved variable to be replaced
+    const VAR_LINK = '%link%';
+    
     // Be called when the plugin is installed
     static function install() {
         // Do nothing
@@ -19,18 +22,43 @@ class BsaCopyright {
     }
     
     static function add_field() {
+        echo '<fieldset><p>';
         BsaUtil::add_checkbox(self::OPTION_COPYRIGHT);
-        // Add the content of copyright info once the option is checked
-        echo '<input name="' . BsaConst::OPTIONS_GROUP . '[' . self::OPTION_INFO . ']" '
-           . 'id="'. self::OPTION_INFO . '" placeholder="Please input the copy right info..." '
-           . 'value="' . BsaUtil::get_value(self::OPTION_INFO) . '" '
-           . 'type="textarea" height="200px" width="500px" /><br />';
+        echo '</p><p>Pls input the content to be displayed as copy right information. %link% is reserved as the link of the article.';
+        echo '</p><p><textarea class="large-text" name="' . BsaConst::OPTIONS_GROUP . '[' . self::OPTION_INFO . ']" '
+           // Enable to input the copyright info only if the option is checked
+           . (BsaUtil::is_enabled(self::OPTION_COPYRIGHT) ? '' : 'disabled')
+           . ' id="'. self::OPTION_INFO . '" placeholder="Please input the copy right info..." rows="3">'
+           . BsaUtil::get_value(self::OPTION_INFO)
+           . '</textarea></p></fieldset><br />';
+?>
+<script type="text/javascript">
+var chkbox = document.getElementById("<?php echo self::OPTION_COPYRIGHT; ?>");
+
+chkbox.onclick = function() {
+    var infoArea = document.getElementById("<?php echo self::OPTION_INFO; ?>");
+    if (chkbox.checked) {
+        infoArea.disabled = false;
+    } else {
+        infoArea.disabled = true;
+    }
+}
+</script>
+<?php
     }
     
     // Add copyright info at the end of the article
     function display_copyright($content) {
-        if (BsaUtil::is_enabled(self::OPTION_COPYRIGHT) && is_singular()) {
-            $content = $content . BsaUtil::get_value(self::OPTION_INFO);
+        if (BsaUtil::is_enabled(self::OPTION_COPYRIGHT) && is_single()) {
+            $copyright = BsaUtil::get_value(self::OPTION_INFO);
+            if (strpos($copyright, self::VAR_LINK)) {
+                // Replace the reserved viarable with blog name and article link
+                $blog_link = '<a href="' . get_permalink() . '">' . get_bloginfo('name') . '</a>';
+                $copyright = str_replace(self::VAR_LINK, $blog_link, $copyright);
+                
+            }
+            
+            $content = $content . $copyright;
         }
         
         return $content;
