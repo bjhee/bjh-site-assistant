b7dca60d0614b70f8a27a052a3ebc38e01d831a2
bjhee <junhe0526@hotmail.com>
2016-02-23 22:42:54 +0800

    Add function that allow user to add script to HTML <head>. It is usually used to add utility code like Baidu Analytics.

--- init.php
+++ init.php
@@ -5,6 +5,7 @@ require_once BSA_PATH . '/func/bjh-avatar.php';
 require_once BSA_PATH . '/func/bjh-meta.php';
 require_once BSA_PATH . '/func/bjh-captcha.php';
 require_once BSA_PATH . '/func/bjh-baidu-submit.php';
+require_once BSA_PATH . '/func/bjh-head-script.php';
 require_once BSA_PATH . '/func/bjh-copyright.php';
 
 // Initialize the option in WordPress database when enabling the plugin
@@ -17,6 +18,7 @@ function bjh_site_assistant_install() {
     BsaMeta::install();
     BsaCaptcha::install();
     BsaBaiduSubmit::install();
+    BsaHeadScript::install();
     BsaCopyright::install();
 
     add_option(BsaConst::OPTIONS_GROUP, $bsa_options);
