466b1ec68447f42a33ae446a0b83e46351b08d34
bjhee <junhe0526@hotmail.com>
2016-03-16 12:20:46 +0800

    1. Allow user to add Back-to-Top button on blogs and pages. 2. Include JS and CSS of this site assistant plugin when enabling it. 3. Fix the bug of wrong url returned by get_plugin_url in util.php.

--- inc/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class BsaConst {
-    // Menu page and options
-    const OPTIONS_PAGE = 'bjh_sa_page';
-    const OPTIONS_SECTION = 'bjh_sa_main';
-    const OPTIONS_GROUP = 'bjh_sa_options';
-    
-}
